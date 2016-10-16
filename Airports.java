e739e0c275bef995f3b1b5e01d5f318940c28b42
snigdhapaka <snigdhapaka@users.noreply.github.com>
2016-10-16 15:41:39 -0400

    added error message in exception, fixed empty airports bug
    if the array list of distances is empty due to missing airports-porto.bin file, findAirports will return empty closest list instead of trying to loop through distances array list which was giving an out of bounds exception

--- Airports.java
+++ Airports.java
@@ -24,41 +24,45 @@ public class Airports extends UnicastRemoteObject implements AirportInterface {
                 airports.add(new AirportInfo(a.getState(), a.getName(), a.getCode(), a.getLat(), a.getLon()));
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("Missing airports-proto.bin file: " + e);
         }
 
         System.out.println("New instance of Airports created");
     }
 
     public ArrayList<AirportDistance> findAirports (double latitude, double longitude) throws RemoteException {
-        double lat1 = Math.toRadians(latitude);
-        double lon1 = Math.toRadians(longitude);
-        ArrayList<AirportDistance> distances = new ArrayList<AirportDistance>();
         
-        // get distance from city to every airport
-        for (AirportInfo airport : airports) {
-            double lon2 = Math.toRadians(airport.getLon());
-            double lat2 = Math.toRadians(airport.getLat());
-
-            double d = 1.1507794*60*Math.toDegrees(Math.acos(Math.sin(lat1)*Math.sin(lat2) + Math.cos(lat1)*Math.cos(lat2)*Math.cos(lon2-lon1)));
-            AirportDistance distance = new AirportDistance(airport, d);
-            distances.add(distance);
-        }
-
-        // sort shortest distance to longest
-        Collections.sort(distances, new Comparator<AirportDistance>() {    
-            @Override
-            public int compare(AirportDistance o1, AirportDistance o2) {
-                if (o1.getDistance() < o2.getDistance()) return -1;
-                if (o1.getDistance() > o2.getDistance()) return 1;            
-                return 0;
-            }
-        });
-
-        // place first 5 in new ArrayList
         ArrayList<AirportDistance> closest = new ArrayList<AirportDistance>();
-        for (int i = 0; i < 5; i++) {
-            closest.add(distances.get(i));
+
+        if(!airports.isEmpty()){
+            double lat1 = Math.toRadians(latitude);
+            double lon1 = Math.toRadians(longitude);
+            ArrayList<AirportDistance> distances = new ArrayList<AirportDistance>();
+            
+            // get distance from city to every airport
+            for (AirportInfo airport : airports) {
+                double lon2 = Math.toRadians(airport.getLon());
+                double lat2 = Math.toRadians(airport.getLat());
+
+                double d = 1.1507794*60*Math.toDegrees(Math.acos(Math.sin(lat1)*Math.sin(lat2) + Math.cos(lat1)*Math.cos(lat2)*Math.cos(lon2-lon1)));
+                AirportDistance distance = new AirportDistance(airport, d);
+                distances.add(distance);
+            }
+
+            // sort shortest distance to longest
+            Collections.sort(distances, new Comparator<AirportDistance>() {    
+                @Override
+                public int compare(AirportDistance o1, AirportDistance o2) {
+                    if (o1.getDistance() < o2.getDistance()) return -1;
+                    if (o1.getDistance() > o2.getDistance()) return 1;            
+                    return 0;
+                }
+            });
+
+            // place first 5 in new ArrayList
+            for (int i = 0; i < 5; i++) {
+                closest.add(distances.get(i));
+            }
         }
         return closest;
     }
