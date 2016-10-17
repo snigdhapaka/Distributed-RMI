62f0ac521a6de84b08143022ff5a0ab194dea171
snigdhapaka <snigdhapaka@users.noreply.github.com>
2016-10-16 23:10:20 -0400

    Update AirportInterface.java

--- AirportInterface.java
+++ AirportInterface.java
@@ -1,8 +1,9 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.io.FileNotFoundException;
 
 import java.util.ArrayList;
 
 public interface AirportInterface extends Remote {
-	public ArrayList<AirportDistance> findAirports (double latitude, double longitude) throws RemoteException;
+	public ArrayList<AirportDistance> findAirports (double latitude, double longitude) throws RemoteException, FileNotFoundException;
 }
